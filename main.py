13dda365c4fefd4da4c2ac93509fc16fd11a8b36
kanadm12 <kanadmukherjee12@gmail.com>
2026-01-18 11:05:47 +0530

    Fix accumulate_grad_batches OmegaConf access

--- main.py
+++ main.py
@@ -617,7 +617,7 @@ if __name__ == "__main__":
             ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
         else:
             ngpu = 1
-        accumulate_grad_batches = lightning_config.trainer.accumulate_grad_batches or 1
+        accumulate_grad_batches = lightning_config.trainer.get("accumulate_grad_batches", 1) or 1
         print(f"accumulate_grad_batches = {accumulate_grad_batches}")
         lightning_config.trainer.accumulate_grad_batches = accumulate_grad_batches
         model.learning_rate = accumulate_grad_batches * ngpu * bs * base_lr
